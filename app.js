ae52d01fd97e1a40d2f3f72ddca71f33ad35c722
gail <luyang.gai@agilysys.com>
2016-06-06 23:28:14 -0700

    small updates

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const eventsCollectionName = 'events';
 const tpsCollectionName = 'ticketPriceSnapshots';
 
 const seatGeekURL = 'https://api.seatgeek.com/2/events?venue.id=13';
-const intervalTimer = 15 * 60 * 1000; //30 minutes
+const intervalTimer = 30 * 60 * 1000; //30 minutes
 
 function cleanupTps(event) {
   for (let key of eventKeysToDelete) {
@@ -43,18 +43,21 @@ function handleJson(json) {
   function callback(db) {
     callbacksMade++;
     if (callbacksMade === callbacksNeeded) {
+      console.log('disconnecting');
       db.close();
     }
   }
 
-  console.log('handleJson called');
+  //console.log('handleJson called');
   mongoClient.connect(`mongodb://localhost:27017/${mongoDbName}`, (err, db) => {
     if (!err) {
-      console.log('We are connected');
+      let currentDate = new Date(Date.now());
+
+      console.log(`We are connected. Inserting at: ${currentDate.toLocaleTimeString()}`);
 
       for (var event of json.events) {
         insertEvents(db,event, callback);
-        insertTPS(db, event, callback);
+        insertTPS(db, event, currentDate, callback);
       }
     } else {
       console.log('failed to connect');
@@ -62,9 +65,8 @@ function handleJson(json) {
   });
 }
 
-function insertTPS(db, tps, callback) {
+function insertTPS(db, tps, currentDate, callback) {
   const tpsCollection = db.collection(tpsCollectionName);
-  let currentDate = new Date(Date.now());
 
   cleanupTps(tps);
   tps['UTC'] = currentDate.toUTCString();
@@ -74,7 +76,7 @@ function insertTPS(db, tps, callback) {
     if (err) {
       console.log(err)
     } else {
-      console.log('one collection insert event passed');
+      //console.log('one collection insert event passed');
     }
     callback(db);
   });
@@ -101,7 +103,6 @@ function insertEvents(db, event, callback) {
       });
     }
     else {
-      console.log(`${event.id} already exists. Not inserting`);
       callback(db);
     }
   });
